1c61067891de5262dbfc8e3b43a17892346a4005
imani.devs <imaniroberts.dev@gmail.com>
2023-05-01 21:17:16 -0400

    add ternary for error

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,19 @@
 import { useState, useEffect } from "react";
 import { getDefinition } from "../services/dictionary-api";
-{
-  /* <FontAwesomeIcon icon={faMagnifyingGlass} />; */
-}
 
 export default function Form() {
-  const [userInput, setUserInput] = useState(""); // the object formData stores info about a definition. defined by a key of 'word' and a empty string value
-  const [word, setWord] = useState(""); // word is
-  const [definition, setDefinition] = useState(null);
+  const [userInput, setUserInput] = useState(""); //userInput is the value of the input. setUserInput is a function that updates the value of the input
+  const [word, setWord] = useState(""); //word is the word that is being searched for. setWord is a function that updates the word that's being searched
+  const [definition, setDefinition] = useState(null); //definition is the definition of the word. setDefinition is a function that updates the definition of the word
 
   const handleChange = (event) => {
     setUserInput(event.target.value); //stores the value of the input and updates user input in real-time
   };
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault(); 
     try {
-      setDefinition(null);
+      setDefinition(null); //
       setWord(userInput);
     } catch (error) {
       console.log(error);
@@ -27,18 +24,19 @@ export default function Form() {
     async function fetchData() {
       if (!word) {
         // if there's no word then stop right here
-        return;
+        return; //stops the function.
       }
       try {
+        // if there is a word then try to get the definition
         const wordDefinition = await getDefinition({ word });
-        setDefinition(wordDefinition[0]);
+        setDefinition(wordDefinition[0]); //set the definition to the first definition in the array
       } catch (error) {
         console.error(error);
-        setDefinition({ word, error: true });
+        setDefinition({ word, error: true }); //word (property) is set to the word that was passed in and error is set to true.
       }
     }
     fetchData();
-  }, [word, setDefinition]); //dependency array. runs once
+  }, [word, setDefinition]); //dependency array. runs once. backup to the if statement
 
   return (
     <div>
@@ -48,37 +46,39 @@ export default function Form() {
           className="searchbar"
           type="text"
           name="input"
-          onChange={handleChange}
-          value={userInput}
-          placeholder="Search for a word"
+          onChange={handleChange} //when the input changes, the handleChange function is called
+          value={userInput} //the value of the input is set to the value of userInput
+          placeholder="Please enter a word"
         />
         <input type="submit" value="search" className="search-btn" />
         <p>
-          Press search to lookup <span className="word-span">{userInput}</span>
+          Press search to look up <span>{userInput}</span>
         </p>
       </form>
-      {definition && definition.error ? <p>bad</p> : null}
+      {definition && definition.error ? ( //if there's a definition and there's an error then show the error message
+        <p>Word not found. Perhaps you misspelled it?</p>
+      ) : null}
+
       {definition && !definition.error && userInput === word ? (
         <div>
-          {/* <h3>Word</h3> */}
-          <p>{definition.word}</p>
-
-          {definition.phonetics[1] ? (
-            <div>
-              <p>{definition.phonetics[1].text}</p>
-              <audio
-                className="audio"
-                controls
-                src={definition.phonetics[1].audio}
-              ></audio>
-            </div>
-          ) : null}
+          {/* <p>{definition.word}</p> */}
+          <div className="definition-container">
+            {definition.phonetics[1] ? (
+              <div>
+                <p>{definition.phonetics[1].text}</p>
+                <audio
+                  className="audio"
+                  controls
+                  src={definition.phonetics[1].audio}
+                ></audio>
+              </div>
+            ) : null}
+          </div>
 
           <p>{definition.meanings[0].partOfSpeech}</p>
 
           <h4>
-            The definition of{" "}
-            <span className="word-span">{userInput || word}</span> is:
+            The definition of <span className="word-span">{userInput}</span> is:
           </h4>
           {definition.meanings[0].definitions.map((definition, index) => (
             <p key={index}>{definition.definition}</p>
