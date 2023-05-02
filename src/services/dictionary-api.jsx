3d7338c32b6e886817fcd4f78da9e9176d214cd8
imani.devs <imaniroberts.dev@gmail.com>
2023-05-02 19:43:15 -0400

    removed comment

--- src/services/dictionary-api.jsx
+++ src/services/dictionary-api.jsx
@@ -2,10 +2,10 @@
 
 import axios from "axios";
 
-export async function getDefinition(formData) { 
+export async function getDefinition(formData) { //formData is an object with the word that's being searched
   try {
     const response = await axios.get(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${formData.word}`
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${formData.word}` //the word that's being searched is passed in here
     );
     const data = response.data; //data is an array of objects with the word and other info about it. 
     return data;
