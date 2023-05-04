6fe022867bee322dda798a7d54a3aae29619de6f
imani.devs <imaniroberts.dev@gmail.com>
2023-05-04 16:34:12 -0400

    updated

--- src/services/dictionary-api.jsx
+++ src/services/dictionary-api.jsx
@@ -1,16 +1,13 @@
-// Purpose: to fetch data from the dictionary api
-
 import axios from "axios";
 
-export async function getDefinition(formData) { //formData is an object with the word that's being searched
+export async function getDefinition(formData) {
   try {
     const response = await axios.get(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${formData.word}` //the word that's being searched is passed in here
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${formData.word}`
     );
-    const data = response.data; //data is an array of objects with the word and other info about it. 
+    const data = response.data;
     return data;
   } catch (error) {
     console.error(error);
   }
 }
-//axios automatically parses the data into json format
